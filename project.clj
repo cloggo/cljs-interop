ce80d13d94faa4961a213eda1e3203fb9d9b484d
Aaron Diep <aaron@delite.ca>
2018-09-18 20:13:45 -0600

    deploy to clojars

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject interop "0.1.0"
+  :description "Extras clojurescript -> javascript interop"
+  :url "https://github.com/cloggo/cljs-interop"
+  :license {:name "MIT" }
+  :source-paths ["src"]
+  :dependencies [[binaryage/oops "0.6.2"]
+                 [org.clojure/clojure "1.8.0"]])
